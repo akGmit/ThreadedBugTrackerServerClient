0577953786217e1cd1b0a1c10a7d6eab2ecc1e2e
akGMIT <g00328529@gmit.ie>
2018-12-23 04:39:14 +0000

    Login, Register, Add bugs, View Bugs functionality implemented

---
+++ BugTrackerServer/BugTrackerServer/src/server/EmployeeData.java
@@ -0,0 +1,66 @@
+package server;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeData {
+	private List<Employee> employeeList;
+	private File file = new File("employeeList.dat");
+	
+	public EmployeeData() {
+		employeeList = new ArrayList<>();
+		loadEmployeeList();
+	}
+	
+	private void loadEmployeeList() {
+		BufferedReader br;
+		Employee emp;
+		try {
+			br = new BufferedReader(new FileReader(file));
+			String line;
+			while((line = br.readLine()) != null) {
+				emp = new Employee();
+				emp.setName(line);
+				emp.setEmpID(br.readLine());
+				emp.setEmail(br.readLine());
+				emp.setPassword(br.readLine());
+				emp.setDepartment(br.readLine());
+				emp.setAssignedBugId(br.readLine());
+				employeeList.add(emp);
+			}
+			br.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	//Add new employee 
+	public void addNewEmployee(Employee emp) {
+		employeeList.add(emp);
+		writeToFile(emp);
+	}
+	
+	
+	private void writeToFile(Employee emp){
+		try {
+			PrintWriter out = new PrintWriter(new FileWriter(file, true), true);
+			out.print(emp.toFile());
+			out.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public List<Employee> getEmployeList() {
+		return employeeList;
+	}
+
+	public void setEmployeList(List<Employee> employeList) {
+		this.employeeList = employeList;
+	}
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+#Multi-Threaded TCP Server and Client for Bug Tracking platform
